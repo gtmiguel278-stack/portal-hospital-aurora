799a48c65567130d104a95403ac93944bdd686b2
gtmiguel278-stack <gtmiguel278@gmail.com>
2026-09-09 18:25:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1157,7 +1157,7 @@ function formatWorkedTime(minutes) {
 // ======================================================
 
 const POINT_REMINDER_INTERVAL =
-  10 * 1000;
+  30 * 60 * 1000;
 
 
 // Controle local do tempo ativo
